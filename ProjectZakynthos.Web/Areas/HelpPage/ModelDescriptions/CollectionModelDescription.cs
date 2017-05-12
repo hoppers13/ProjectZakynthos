4f16096bb9bdd08ce31d66ceb381d6b3fe1151fd
Gianluca <Gianluca>
2017-05-12 09:44:52 +0100

    Add .NET web API framework

---
+++ ProjectZakynthos.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ProjectZakynthos.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
