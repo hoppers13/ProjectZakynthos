c9fdc4a916f499c0c3a7e60870984a52cf678f68
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-05-31 06:49:06 +0100

    Add login

--- ProjectZakynthos.Web/Areas/Demo/Controllers/WishController.cs
+++ ProjectZakynthos.Web/Areas/Demo/Controllers/WishController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace ProjectZakynthos.Web.Areas.Demo.Controllers
 {
@@ -8,6 +12,6 @@ namespace ProjectZakynthos.Web.Areas.Demo.Controllers
         public ActionResult Index()
         {
             return View();
-        }        
+        }
     }
 }
