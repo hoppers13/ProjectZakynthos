c1e8e63486d37f4c216889287f0ee5ae1dbee0e6
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-06-04 21:46:30 +0100

    Move wish controller to stamps area

---
+++ ProjectZakynthos.Web/Areas/stamps/Controllers/WishController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectZakynthos.Web.Areas.stamps.Controllers
+{
+	public class WishController : Controller
+    {
+
+		[Authorize]
+        // GET: stamps/Wish
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
