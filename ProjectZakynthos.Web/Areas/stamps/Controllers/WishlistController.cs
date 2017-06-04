eb1d6afd452dd5ef26c8b802599978af3b1121ae
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-06-04 21:35:18 +0100

    Add "stamps" area Add manage wishlist

---
+++ ProjectZakynthos.Web/Areas/stamps/Controllers/WishlistController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectZakynthos.Web.Areas.stamps.Controllers
+{
+	[Authorize]
+    public class WishlistController : Controller
+    {
+        // GET: stamps/Wishlist
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Manage()
+		{
+			return View();
+		}
+    }
+}
---
+++ ProjectZakynthos.Web/Areas/stamps/stampsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectZakynthos.Web.Areas.stamps
+{
+    public class stampsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "stamps";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "stamps_default",
+                "stamps/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
