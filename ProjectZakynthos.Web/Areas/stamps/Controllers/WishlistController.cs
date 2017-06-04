2a52406cf30e17e5f40a554edf18384050a404c8
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-06-04 23:28:51 +0100

    Manage wishilist

--- ProjectZakynthos.Web/Areas/stamps/Controllers/WishlistController.cs
+++ ProjectZakynthos.Web/Areas/stamps/Controllers/WishlistController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using ProjectZakynthos.Web.Areas.stamps.Models;
+using System.Collections.Generic;
 
 namespace ProjectZakynthos.Web.Areas.stamps.Controllers
 {
@@ -11,9 +13,30 @@ namespace ProjectZakynthos.Web.Areas.stamps.Controllers
             return View();
         }
 
+		[HttpGet]
 		public ActionResult Manage()
 		{
-			return View();
+			var model = new WishlistViewModel
+			{
+				Items = new List<PhilatelicItem>
+				{
+					new PhilatelicItem{Country = "GB"},
+					new PhilatelicItem{Country = "GB"},
+					new PhilatelicItem{Country = "GB"},
+					new PhilatelicItem{Country = "GB"},
+					new PhilatelicItem{Country = "GB"},
+					new PhilatelicItem{Country = "GB"}					
+				}
+			};
+			
+			return View(model);
 		}
+
+		[HttpPost]
+		public ActionResult Manage(WishlistViewModel model)
+		{
+			return View(model);
+		}
+
     }
 }
---
+++ ProjectZakynthos.Web/Areas/stamps/Models/WishlistViewmodel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectZakynthos.Web.Areas.stamps.Models
+{
+	public class WishlistViewModel
+	{
+		public IEnumerable<PhilatelicItem> Items { get; set; }
+	}
+
+	public class PhilatelicItem
+	{
+		public string Country { get; set; }
+		public int Year { get; set; }
+		public string FaceValue { get; set; }
+		public string Description { get; set; }
+	}
+}
