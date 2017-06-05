e54535bd139283248c678fc466a025b04fcbf6de
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-06-05 13:06:15 +0100

    Add in memory persistence

---
+++ ProjectZakynthos.Presistence.InMemory/WishlistRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjectZakynthos.Persistence;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectZakynthos.Presistence.InMemory
+{
+	public class WishlistRepository : IWishlistRepository
+	{
+		public IEnumerable<string> GetWishlist()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
